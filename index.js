79348823dbb8ff36f14a9a266bd4d521318860d8
Krupa Sivakumaran <krupa32@gmail.com>
2012-07-05 17:49:11 +0530

    Added some effects to landing page

--- index.js
+++ index.js
@@ -14,28 +14,41 @@ var app = {
 		});
 		
 		$('#apply').click(app.upload_resume);
+		
+		$('#back').click(function(){
+			$('#form_preferences').hide('slide', { direction: 'right' }, 'fast', function(){
+				$('#form_resume').show('slide', { direction: 'left' });
+			});
+			return false;
+		})
 
 	},
 	
 	upload_resume: function() {
-		//debugger;
+
 		var fd = new FormData();
 		var resume_file = $('#resume').kfilepicker('option', 'file_data');
 		
-		console.log('upload_resume');
 		fd.append('resume', resume_file);
 		fd.append('email', $('#email').val());
 		fd.append('experience', $('#experience').kslider('option', 'value'));
 
-		console.log('calling ajax. email=' + $('#email').val() + " exp=" + $('#experience').kslider('option', 'value'));
 		$.ajax({
 			url: 'upload_resume.php',
 			type: 'POST',
 			data: fd,
 			processData: false,
 			contentType: false,
-			success: function(resp){
+			success: function(data){
+				resp = JSON.parse(data);
 				console.log('remote_upload received: ' + resp);
+				if (resp !== true) {
+					alert("There was an error uploading your resume. [" + resp + "]");
+					return;
+				}
+				$('#form_resume').hide('slide', { direction: 'left' }, 'fast', function(){
+					$('#form_preferences').show('slide', { direction: 'right' });
+				});
 			},
 		});
 		
--- upload_resume.php
+++ upload_resume.php
@@ -11,13 +11,33 @@
 	$email = $_POST["email"];
 	$experience = $_POST["experience"];
 	
-	$q = "insert into candidates set email='$email', experience=$experience";
-	if (!$db->query($q)) {
-		$ret = "Error creating candidate record";
+	/* check if this user already exists in our db */
+	$q = "select id from candidates where email='$email'";
+	if (!($result = $db->query($q))) {
+		$ret = "Error checking if user exists";
 		goto out;
 	}
 	
-	$id = $db->insert_id;
+	if ($result->num_rows == 0) {
+		/* user does not exist, create a new record */
+		$q = "insert into candidates set email='$email', experience=$experience";
+		if (!$db->query($q)) {
+			$ret = "Error creating candidate record";
+			goto out;
+		}
+		/* get the generated id */
+		$id = $db->insert_id;
+	} else {
+		/* user already found in db, update the experience and use the id */
+		$row = $result->fetch_assoc();
+		$id = $row["id"];
+		error_log("User $email found. Using id $id");
+		
+		$q = "update candidates set experience=$experience where id=$id";
+		$db->query($q);
+	}
+	
+
 	
 	/* all resume uploads are stored in /uploads.
 	 * this dir should be created with write permission for the apache user/group (www-data)
